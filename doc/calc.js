f894b0f24679b9bbc0b72ef51495af04968bdf28
Shiwam07 <shiwamrawat413@gmail.com>
2021-12-31 00:44:42 +1100

    addedFunctionalities

---
+++ doc/calc.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $(".displayValue").click(function () {
+    const numValue = $(this).text();
+    $("#display").append(numValue);
+  });
+  $(".operator").click(function () {
+    const operator = $(this).text();
+    $("#display").append(operator);
+  });
+  $(".del").click(function () {
+    const displayValue = $("#display").text();
+    const slicedValue = displayValue.slice(0, displayValue.length - 1);
+    $("#display").text(slicedValue);
+  });
+  $(".equals").click(function () {
+    const displayedValue = $("#display").text();
+    function calculate(value) {
+      return new Function("return " + value)();
+    }
+    try {
+      const calculatedValue = calculate(displayedValue);
+      $("#calculate").text(calculatedValue);
+    } catch (error) {
+      $("#calculate").text("Error");
+    }
+    $("#display").text("");
+  });
+});
